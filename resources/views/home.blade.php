a63e3afe8ea055e44161af07eac916d8c8f37e37
Lucho <Lucianodeon@gmail.com>
2019-11-12 09:39:37 -0300

    lala

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+  <!DOCTYPE html>
+  <html lang="en" dir="ltr">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="css/styles.css">
+      <link href="https://fonts.googleapis.com/css?family=Anton|Chilanka|Lobster&display=swap" rel="stylesheet">
+      <title>BEER HOUR</title>
+      <title></title>
+    </head>
+    <body class="user">
+    
+      <main class="usar">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    You are logged in!
-                </div>
-            </div>
+      <div class="content">
+        <div class="content-nav">
+        <img src="img/pp.jpg" alt="foto2"><h2>juancho-kpo</h2>
         </div>
+        <div class="content-img">
+          <img src="img/alala.jpg" alt="foto2">
+        </div>
+        <div class="content-pie">
+          <h3>Aca tirando magia</h3>
+        </div>
+
+      </div>
+      <div class="content">
+        <div class="content-nav">
+        <img src="img/pp.jpg" alt="foto2"><h2>juancho-kpo</h2>
+        </div>
+        <div class="content-img">
+          <img src="img/biru.jpeg" alt="foto2">
+        </div>
+        <div class="content-pie">
+          <h3>ビールを飲みましょうか</h3>
+        </div>
+      </div>
+
+      </main>
+    </body>
     </div>
-</div>
+  </html>
+
+
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,14 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
+        <link rel="stylesheet" href="css/styles.css">
+        <link href="https://fonts.googleapis.com/css?family=Anton|Chilanka|Lobster&display=swap" rel="stylesheet">
+        <title>BEER HOUR</title>
+                <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
+        {{-- <style>
             html, body {
                 background-color: #fff;
                 color: #636b6f;
@@ -61,7 +61,8 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
-        </style>
+        </style> --}}
+
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -79,20 +80,29 @@
                 </div>
             @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+            <div class="beerhour">
+            <img src="img/logo.png" alt="logo_beerhour">
+            <h1>beer hour</h1>
+            <h2>Don´t Worry Beer Happy</h2>
+            </div>
+            <div class="text3" id="distinto">
+              <img src="img/botellas-cerveza.png" alt="foto1r">
+              <h2>¿Queres variedad?</h2>
+              <h3>¡Encuentra a los Brewers que se ajusten a tus gustos!</h2>
+            </div>
+            <div class="text3">
+              <img src="img/_DSC0202.jpg" alt="foto2">
+              <h2>¿Buscando gente a quien enseñarle tu cerveza?</h2>
+              <h3>¡Aqui te encontraran ellos primero!</h2>
+            </div>
+            <div class="text3">
+              <img src="img/mesa-vaso.png" alt="fot3">
+              <h2>¿Sos mayorista?</h2>
+              <h3>¡Este es tu lugar!</h3>
+            <a href="{{ route('register') }}">Register</a>
+            </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+
             </div>
         </div>
     </body>
