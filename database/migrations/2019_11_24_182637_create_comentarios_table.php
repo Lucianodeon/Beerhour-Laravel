08f3e86a52d07d44db3add669ec213b9ccf1e169
Lucho <Lucianodeon@gmail.com>
2019-11-24 15:46:42 -0300

    migraciones

---
+++ database/migrations/2019_11_24_182637_create_comentarios_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComentariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comentarios', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_user');
+            $table->integer('id_post');
+            $table->string('descripcion');
+            $table->integer('like');
+
+
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comentarios');
+    }
+}
